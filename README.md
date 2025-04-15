691605144eb8909e4ef1ff8d2fb436cb0d864546
bikashneupane17 <edubikkash@gmail.com>
2025-04-15 19:36:21 +1000

    Search and Sorting working

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. bootstrap, folder structure
 2. firebase, firestore added and hosted
 3. csv file is parsed with papaparse and firestore is loaded with this data
-4.
+4. automobile service added , basic UI displaying all the data
+5. Search/ Sort Feature
--- src/app/automobile/automobile.component.ts
+++ src/app/automobile/automobile.component.ts
@@ -12,12 +12,33 @@ import { Automobile } from '../model/automobile.model';
 })
 export class AutomobileComponent implements OnInit {
   automobiles: Automobile[] = [];
+  searchTerm = '';
+  sortField = 'name';
+  sortOrder = 'asc';
 
   constructor(private automobileService: AutomobileService) {}
 
+  // Initial Component Load
   ngOnInit(): void {
     this.automobileService.getAutomobiles().subscribe((data) => {
       this.automobiles = data;
+    });
+  }
+
+  // Filter automobiles based on search field, sort field and sort order
+  filterdAutomobiles(): Automobile[] {
+    let filtered = this.automobiles.filter((automobile) =>
+      automobile?.name
+        .toLowerCase()
+        .includes(this.searchTerm.trim().toLowerCase())
+    );
+
+    return filtered.sort((a, b) => {
+      const valA = (a as any)[this.sortField];
+      const valB = (b as any)[this.sortField];
+
+      if (this.sortOrder === 'asc') return valA > valB ? 1 : -1;
+      return valA < valB ? 1 : -1;
     });
   }
 }
