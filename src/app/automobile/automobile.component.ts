0c3e22bb9fc21910ba197086df89967976943129
bikashneupane17 <edubikkash@gmail.com>
2025-04-15 18:42:14 +1000

    automobile service added, basic UI added

--- src/app/automobile/automobile.component.ts
+++ src/app/automobile/automobile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AutomobileService } from '../services/automobile/automobile.service';
+import { Automobile } from '../model/automobile.model';
 
 @Component({
   selector: 'app-automobile',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './automobile.component.html',
   styleUrl: './automobile.component.scss',
 })
-export class AutomobileComponent {}
+export class AutomobileComponent implements OnInit {
+  automobiles: Automobile[] = [];
+
+  constructor(private automobileService: AutomobileService) {}
+
+  ngOnInit(): void {
+    this.automobileService.getAutomobiles().subscribe((data) => {
+      this.automobiles = data;
+    });
+  }
+}
--- src/app/services/automobile/automobile.service.ts
+++ src/app/services/automobile/automobile.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Automobile } from '../../model/automobile.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutomobileService {
+  constructor(private firestore: Firestore) {}
 
-  constructor() { }
+  getAutomobiles(): Observable<Automobile[]> {
+    const automobileCollection = collection(this.firestore, 'automobile');
+    return collectionData(automobileCollection, {
+      idField: 'id',
+    }) as Observable<Automobile[]>;
+  }
 }
